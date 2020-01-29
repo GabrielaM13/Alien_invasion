7992f72e6c09094a63bd5a90fe745ccd7fea97d1
GabrielaM13 <gabrrielam13@gmail.com>
2020-01-29 23:29:25 +0100

    Refactored the ship's code to use the settings. Defined the speed in the settings and used its value instead of magic number

--- ship.py
+++ ship.py
@@ -8,6 +8,7 @@ class Ship:
         """ Initialize the ship and set its starting position """
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # Load the ship image and set its rect
         img_path = self.get_relative_path("images/ship.bmp")
@@ -16,16 +17,18 @@ class Ship:
 
         # Start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
 
         # Movement flag
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
-        if self.moving_right:
-            self.rect.x += 1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
         if self.moving_left and self.rect.x > 0:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed            
+        self.rect.x = self.x
 
     def blitme(self):
         """ Draw the ship at its current position """
