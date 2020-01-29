4ffabb41995bc7e16194d62deb4b5d439b03abf5
GabrielaM13 <gabrrielam13@gmail.com>
2020-01-29 22:48:35 +0100

    Moving the ship to left and right with each arrow key press

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,17 +21,28 @@ class AlienInvasion:
     def run_game(self):
         """ Start the main loop for the game """
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            # Redraw the screen during each pass trough the loop
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            
-            # Make the most recently drawn screen visible
-            pygame.display.flip()
+    def _check_events(self):
+        """ Respond to keypressed and mouse events """
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move this ship to the right
+                    self.ship.rect.x += 15
+                elif event.key == pygame.K_LEFT:
+                    self.ship.rect.x -= 15
+
+    def _update_screen(self):
+        """" Redraw the screen during each pass trough the loop """
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        
+        # Make the most recently drawn screen visible
+        pygame.display.flip()
 
 if __name__ == "__main__":
     # Make a game instance and run the game
