3985c9c3d79e3f659f9de81071664d33a309abd2
GabrielaM13 <gabrrielam13@gmail.com>
2020-01-29 23:10:29 +0100

    Added smooth movement to left and right. Made it stop at left border of the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,6 +22,7 @@ class AlienInvasion:
         """ Start the main loop for the game """
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -31,10 +32,14 @@ class AlienInvasion:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    # Move this ship to the right
-                    self.ship.rect.x += 15
+                    self.ship.moving_right = True
                 elif event.key == pygame.K_LEFT:
-                    self.ship.rect.x -= 15
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """" Redraw the screen during each pass trough the loop """
--- ship.py
+++ ship.py
@@ -17,6 +17,16 @@ class Ship:
         # Start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        if self.moving_right:
+            self.rect.x += 1
+        elif self.moving_left and self.rect.x > 0:
+            self.rect.x -= 1
+
     def blitme(self):
         """ Draw the ship at its current position """
         self.screen.blit(self.image, self.rect)
