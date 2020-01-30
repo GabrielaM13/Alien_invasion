bb6b3e162ee80c741c2573133a260a7e6ab813db
GabrielaM13 <gabrrielam13@gmail.com>
2020-01-30 21:13:28 +0100

    Added method to display the game in full mode. Didn't call the method as full mode looks weird on my machine (ship is almost completely hidden

--- alien_invasion.py
+++ alien_invasion.py
@@ -58,6 +58,12 @@ class AlienInvasion:
         # Make the most recently drawn screen visible
         pygame.display.flip()
 
+    def set_fullscreen(self):
+        """ Sets the game in fullscreen mode """
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+
 if __name__ == "__main__":
     # Make a game instance and run the game
     ai = AlienInvasion()
