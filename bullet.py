cb5b538ffb866b1948f508760014accd311bef92
GabrielaM13 <gabrrielam13@gmail.com>
2020-01-30 22:20:03 +0100

    Added functionality to shoot rocks or bullets, depending on the settings

--- bullet.py
+++ bullet.py
@@ -10,25 +10,34 @@ class Bullet(Sprite):
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.color = self.settings.bullet_color
-
-        """
-        # Create a bullet rect at (0, 0) and then set correct position
-        self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
-        """
        
-        # Load the ship image and set its rect
-        img_path = self.settings._get_relative_path("images/bullet.bmp")
-        self.image = pygame.image.load(img_path)
-        self.rect = self.image.get_rect()
-
+        self.initialize()
 
         # Start each new ship at the bottom center of the screen
         self.rect.midtop =  ai_game.ship.rect.midtop
         self.y = float(self.rect.y)
 
+    def initialize(self):
+        if self.settings.bullet_type == "sprite":
+            self.initialize_sprite()
+        else:
+            self.initialize_rock()
+
+    def initialize_sprite(self):
+         # Create a bullet rect at (0, 0) and then set correct position
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
+
+    def initialize_rock(self):
+        # Load the ship image and set its rect
+        img_path = self.settings._get_relative_path("images/bullet.bmp")
+        self.image = pygame.image.load(img_path)
+        self.rect = self.image.get_rect()
+
     def draw_bullet(self):
-        # pygame.draw.rect(self.screen, self.color, self.rect)
-        self.blitme()
+        if self.settings.bullet_type == "sprite":
+           pygame.draw.rect(self.screen, self.color, self.rect)
+        else:
+            self.blitme()
 
     def update(self):
         self.y -= self.settings.bullet_speed
--- settings.py
+++ settings.py
@@ -14,6 +14,8 @@ class Settings:
         self.ship_speed = 1.5
 
         # Bullet settings
+        self.bullet_types = ["sprite", "rocks"]
+        self.bullet_type = "sprite"
         self.bullet_speed = 1.0
         self.bullet_width = 3
         self.bullet_height = 15
