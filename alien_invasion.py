9c6f3c57cd5fc68290d31359ed84561b08d914ee
GabrielaM13 <gabrrielam13@gmail.com>
2020-02-02 19:15:09 +0100

    Reset the game components before starting a new game

--- alien_invasion.py
+++ alien_invasion.py
@@ -90,7 +90,17 @@ class AlienInvasion:
 
     def _check_play_button(self, mouse_pos):
         if self.play_button.rect.collidepoint(mouse_pos):
+            # Reset the game statistics.
+            self.stats.reset_stats()
             self.stats.game_active = True
+
+            # Get rid of any remaining aliens and bullets
+            self.aliens.empty()
+            self.bullets.empty()
+
+            # Create a new fleet and center the ship
+            self._create_fleet()
+            self.ship.center_ship()
 
     def _check_keydown_events(self, event):
         """ Respond to keypresses """
