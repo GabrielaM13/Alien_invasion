2fce581a7d782330384dcc314000adb107d32012
GabrielaM13 <gabrrielam13@gmail.com>
2020-02-04 21:23:19 +0100

    Added labels for the score, high score and level

--- scoreboard.py
+++ scoreboard.py
@@ -12,7 +12,7 @@ class Scoreboard:
 
         # Font settings for scoring information.
         self.text_color = (30, 30, 30)
-        self.font = pygame.font.SysFont(None, 48)
+        self.font = pygame.font.SysFont(None, 35)
 
         # Prepare the initial score images.
         self.prep_score()
@@ -21,7 +21,7 @@ class Scoreboard:
     def prep_score(self):
         """ Turned the score into a rendered image """
         rounded_score = round(self.stats.score, -1)
-        score_str = "{:,}".format(rounded_score)
+        score_str = "Score: " + "{:,}".format(rounded_score)
         self.score_image = self.font.render(score_str, True, self.text_color, self.settings.bg_color)
 
         # Display the score at the top right of the screen
@@ -32,7 +32,7 @@ class Scoreboard:
     def prep_high_score(self):
         """ Turned the high score into a rendered image """
         high_score = round(self.stats.high_score, -1)
-        high_score_str = "{:,}".format(high_score)
+        high_score_str = "High score: " "{:,}".format(high_score)
         self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color)
 
         # Center the high score at the top of the screen
@@ -42,7 +42,7 @@ class Scoreboard:
 
     def prep_level(self):
         """ Turned the level into a rendered image """
-        level_str = str(self.stats.level)
+        level_str = "Level: " + str(self.stats.level)
         self.level_image = self.font.render(level_str, True, self.text_color, self.settings.bg_color)
 
         # Position the level below the score
